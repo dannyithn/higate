db1e0424d37e31fda4450d5aa5de99b5b642e7ec
Danny <dungithn@gmail.com>
2022-12-29 00:20:22 +0700

    Update Post content

--- src/components/Sticky.vue
+++ src/components/Sticky.vue
@@ -1,49 +1,66 @@
 <template>
-  <div class="blog-sticky"
-      v-for="blog in blogSticky"
-        v-bind:key="blog.sys.id"
-    >
+  <div class="blog-sticky" v-for="blog in blogSticky" v-bind:key="blog.sys.id">
     <div class="bg-[#0B1992] text-secondary font-bold">
-        <div class="px-4 md:px-[30px] max-w-[1062px] 1440:max-w-[1128px] lg:px-0 m-auto pt-[41px] pb-[58px] lg:pt-[70px] lg:pb-[342px] text-center lg:text-left">
-          <h1 class="text-[36px] leading-[120%] 1440:text-[64px] Open Sans mb-4">Blog</h1>
-          <p>See what’s going on at HiGate and in the technology world.</p>
-        </div>
+      <div
+        class="px-4 md:px-[30px] max-w-[1062px] xl:max-w-[1128px] lg:px-0 m-auto pt-[41px] pb-[58px] lg:pt-[70px] lg:pb-[342px] text-center lg:text-left"
+      >
+        <h1 class="text-[36px] leading-[120%] xl:text-[64px] Open Sans mb-4">
+          Blog
+        </h1>
+        <p>See what’s going on at HiGate and in the technology world.</p>
       </div>
-      <div class="px-4 md:px-[30px] max-w-[1062px] 1440:max-w-[1128px] lg:px-0 mx-auto lg:mt-[-260px] text-secondary">
-        <div class="md:relative">
-          
-          <img class="m-auto w-[100%] aspect-[1127/520]" :src="blog.blogImage.url" :alt="blog.blogImage.title" :width="blog.blogImage.width" :height="blog.blogImage.height"  />
-          <div class="md:absolute top-[50%] lg:left-[100px] translate-y-[-50%] lg:max-w-[495px]">
-            <h3 class="my-0 text-[28px] leading-[120%] lg:text-[36px] font-bold Open Sans">
-              <a href="">{{ blog.blogTitle  }}</a>
+    </div>
+    <div
+      class="px-4 md:px-[30px] max-w-[1062px] xl:max-w-[1128px] lg:px-0 mx-auto lg:mt-[-260px] text-secondary"
+    >
+      <div class="md:relative">
+        <img
+          class="m-auto w-[100%] aspect-[1127/520]"
+          :src="blog.blogImage.url"
+          :alt="blog.blogImage.title"
+          :width="blog.blogImage.width"
+          :height="blog.blogImage.height"
+        />
+        <div
+          class="md:absolute top-[50%] lg:left-[100px] translate-y-[-50%] lg:max-w-[495px]"
+        >
+          <h3
+            class="my-0 text-[28px] leading-[120%] lg:text-[36px] font-bold Open Sans"
+          >
+          <router-link :to="'/post/' + blog.sys.id">
+              {{ blog.blogTitle }}
+            </router-link>
           </h3>
-            <p class="text-4 leading-[143%] mt-6 mb-6 lg:mt-6 lg:mb-4"></p>
-            <p class="">{{ blog.createdDate  }}</p>
-            <div class="border border-secondary py-2 px-[22px] rounded-lg w-fit truncate mt-10"><a href="">See More</a></div>
+          <p class="text-4 leading-[143%] mt-6 mb-6 lg:mt-6 lg:mb-4"></p>
+          <p class="">{{ getFormatDate(blog.createdDate) }}</p>
+          <div
+            class="border border-secondary py-2 px-[22px] rounded-lg w-fit truncate mt-10"
+          >
+          <router-link :to="'/post/' + blog.sys.id">See More</router-link>
           </div>
         </div>
       </div>
-      
+    </div>
   </div>
 </template>
-  
-  <script>
-  // @ is an alias to /src
-  export default {
-    name: 'Sticky',
-    components: {
-    },
-    data() {
-        return {
-            blogSticky: []
-        };
-      },
-    async created() {
-        this.blogSticky = await this.getBlogSticky();
-      },
-    methods: {
-        getBlogSticky: async () => {
-          const query = `{
+
+<script>
+import moment from "moment";
+// @ is an alias to /src
+export default {
+  name: "Sticky",
+  components: {},
+  data() {
+    return {
+      blogSticky: [],
+    };
+  },
+  async created() {
+    this.blogSticky = await this.getBlogSticky();
+  },
+  methods: {
+    getBlogSticky: async () => {
+      const query = `{
             blogStickyCollection{
               items {
                 sys {
@@ -71,36 +88,34 @@
               }
             }
           }`;
-          const fetchUrl = `https://graphql.contentful.com/content/v1/spaces/${process.env.VUE_APP_CONTENTFUL_SPACE_ID}`;
-          const fetchOptions = {
-            method: "POST",
-            headers: {
-              Authorization: `Bearer ${process.env.VUE_APP_CONTENTFUL_ACCESS_TOKEN}`,
-              "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ query })
-          };
-  
-          try {
-            const response = await fetch(fetchUrl, fetchOptions).then(response =>
-              response.json()
-            );
-            return response.data.blogStickyCollection.items;
-          } catch (error) {
-            throw new Error("Could not receive the data from Contentful!");
-          }
+      const fetchUrl = `https://graphql.contentful.com/content/v1/spaces/${process.env.VUE_APP_CONTENTFUL_SPACE_ID}`;
+      const fetchOptions = {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.VUE_APP_CONTENTFUL_ACCESS_TOKEN}`,
+          "Content-Type": "application/json",
         },
-        format_date(value){
-          if (value) {
-            return moment(String(value)).format('YYYY MM DD')
-            }
-        }
-      },
-    mounted () {
-      document.body.classList.add('font-body')
+        body: JSON.stringify({ query }),
+      };
+
+      try {
+        const response = await fetch(fetchUrl, fetchOptions).then((response) =>
+          response.json()
+        );
+        return response.data.blogStickyCollection.items;
+      } catch (error) {
+        throw new Error("Could not receive the data from Contentful!");
+      }
     },
-    destroyed () {
-      document.body.classList.remove('font-body')
-    }
-  }
-  </script>
+    getFormatDate: function (date) {
+      return moment(date, "YYYY-MM-DD").format("DD/MM/YYYY");
+    },
+  },
+  mounted() {
+    document.body.classList.add("font-body");
+  },
+  destroyed() {
+    document.body.classList.remove("font-body");
+  },
+};
+</script>
--- src/views/PostDetail.vue
+++ src/views/PostDetail.vue
@@ -2,7 +2,7 @@
   <div class="post">
     <div class="bg-bg-blue">
       <div
-        class="px-4 768:px-[30px] max-w-[1062px] xl:max-w-[1128px] lg:px-0 m-auto pt-[41px] pb-[58px] lg:pt-[70px] lg:pb-[98px] text-center lg:text-left text-secondary"
+        class="px-4 md:px-[30px] max-w-[1062px] xl:max-w-[1128px] lg:px-0 m-auto pt-[41px] pb-[58px] lg:pt-[70px] lg:pb-[98px] text-center lg:text-left text-secondary"
       >
         <h1
           class="text-[36px] leading-[120%] xl:text-[64px] font-bold Open Sans mb-4"
@@ -12,33 +12,37 @@
         <p>See what’s going on at HiGate and in the technology world.</p>
       </div>
     </div>
-    <div v-for="post in posts" v-bind:key="$route.params.id">
-      <div v-if="post.sys.id === $route.params.id">
-        <div
-          class="px-4 md:px-[30px] max-w-[1062px] xl:max-w-[1128px] lg:px-0 m-auto py-12 lg:py-[96px]"
-        >
-          <div class="lg:flex justify-between">
-            <div class="w-[100%] lg:w-[17%] hidden lg:block">
-              <p class="dark:text-body-text text-[14px] leading-[20px]">
-                written by
-                <span class="text-title-text ml-2">{{ post.blogAuthor }}</span>
-              </p>
-              <p class="dark:text-body-text text-[14px] leading-[20px] mt-2">
-                <span>{{ getFormatDate(post.createdDate) }}</span>
-              </p>
-              <ul
-                class="list-none flex align-center flex-wrap gap-y-2 gap-x-2 mt-[16px] lg:mt-6"
-              ></ul>
+    <div>
+      <div
+        class="px-4 md:px-[30px] max-w-[1062px] xl:max-w-[1128px] lg:px-0 m-auto py-12 lg:py-[96px]"
+      >
+        <div class="lg:flex justify-between">
+          <div class="w-[100%] lg:w-[17%] hidden lg:block">
+            <p class="dark:text-body-text text-[14px] leading-[20px]">
+              written by
+              <span class="text-title-text ml-2">{{
+                dataPost?.fields?.blogAuthor
+              }}</span>
+            </p>
+            <p class="dark:text-body-text text-[14px] leading-[20px] mt-2">
+              {{ getFormatDate(dataPost?.fields?.createdDate) }}
+            </p>
+            <ul
+              class="list-none flex align-center flex-wrap gap-y-2 gap-x-2 mt-[16px] lg:mt-6"
+            ></ul>
+          </div>
+          <div class="w-[100%] lg:w-[83%] dark:text-title-text">
+            <h2
+              class="text-[28px] leading-[120%] lg:text-[36px] font-bold Open Sans"
+            >
+              {{ dataPost?.fields?.blogTitle }}
+            </h2>
+            <div v-for="(items, index) in dataPost?.fields?.blogContent?.content" :key="index">
+              <div v-for="(item, index) in items.content" :key="index">
+                {{ item.value }}
+              </div>
             </div>
-            <div class="w-[100%] lg:w-[83%] dark:text-title-text">
-              <h2
-                class="text-[28px] leading-[120%] lg:text-[36px] font-bold Open Sans"
-              >
-                {{ post.blogTitle }}
-              </h2>
-                    {{ convertContent(post.blogContent.json.content) }}
-              <!-- <div v-html=post.blogContent.json.content></div> -->
-            </div>
+            
           </div>
         </div>
       </div>
@@ -48,81 +52,49 @@
 
 <script>
 import moment from "moment";
-import Sticky from "@/components/Sticky.vue";
+const contentful = require("contentful");
+
 export default {
   name: "PostDetail",
+  props: ["id"],
   components: {},
   data() {
     return {
-      posts: [],
+      dataPost: [],
     };
   },
-  async created() {
-    this.posts = await this.getPosts();
-  },
   methods: {
-    getPosts: async () => {
-      const query = `{
-          blogCollection{
-            items {
-              sys {
-                id
-              }
-              blogTitle
-              blogImage {
-                title
-                description
-                contentType
-                fileName
-                size
-                url
-                width
-                height
-              }
-              blogSummary
-              createdDate
-              blogAuthor
-              blogTags
-              public
-              blogContent {
-                json
-              }
-            }
-          }
-        }`;
-      const fetchUrl = `https://graphql.contentful.com/content/v1/spaces/${process.env.VUE_APP_CONTENTFUL_SPACE_ID}`;
-      const fetchOptions = {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${process.env.VUE_APP_CONTENTFUL_ACCESS_TOKEN}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ query }),
-      };
-
+    async getDataModel() {
+      const client = contentful.createClient({
+        space: process.env.VUE_APP_CONTENTFUL_SPACE_ID,
+        accessToken: process.env.VUE_APP_CONTENTFUL_ACCESS_TOKEN,
+      });
       try {
-        const response = await fetch(fetchUrl, fetchOptions).then((response) =>
-          response.json()
-        );
-        return response.data.blogCollection.items;
+        await client.getEntry(this.$route.params.id).then((entries) => {
+          console.log(entries);
+          this.dataPost = entries;
+        });
       } catch (error) {
-        throw new Error("Could not receive the data from Contentful!");
+        this.dataPost = undefined;
+        console.log(error);
       }
     },
     getFormatDate: function (date) {
       return moment(date, "YYYY-MM-DD").format("DD/MM/YYYY");
     },
     convertContent(data) {
-        var html = "";
-      for (var i = 0; i < data.length; i++) {
+      console.log(data);
+      var html = "";
+      for (var i = 0; data < 10; i++) {
         for (var j = 0; j < data[i].content.length; j++) {
           html += data[i].content[j].value;
         }
       }
       return html;
-    }
+    },
   },
   mounted() {
+    this.getDataModel();
     document.body.classList.add("font-body");
   },
   destroyed() {
